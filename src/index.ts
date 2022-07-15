3c6052e4febc0e2b97117b3ce51891295cc35519
Fedor Volkov <f.volkov10@gmail.com>
2022-07-15 15:34:19 +0300

    write promises

---
+++ src/index.ts
@@ -0,0 +1,2 @@
+export { visible, hidden } from "./visibilityChange";
+export { default } from "./visibilityChange";
---
+++ src/visibilityChange.ts
@@ -0,0 +1,33 @@
+function isVisiblePromise(): Promise<{
+  result: boolean;
+  listener: (this: Document, ev: Event) => any;
+}> {
+  return new Promise(function (resolve) {
+    const listener = () => {
+      resolve({ result: !document.hidden, listener });
+    };
+    document.addEventListener("visibilitychange", listener);
+  });
+}
+
+export default async function visibilityChange() {
+  const result = await isVisiblePromise();
+  document.removeEventListener("visibilitychange", result.listener);
+  return result.result;
+}
+
+export async function visible() {
+  let result = !document.hidden;
+  while (!result) {
+    result = await visibilityChange();
+  }
+  return result;
+}
+
+export async function hidden() {
+  let result = document.hidden;
+  while (!result) {
+    result = !(await visibilityChange());
+  }
+  return result;
+}
